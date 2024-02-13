fa2a76f58804ee838edd22cd2dcf623c42ffc1a1
Liam <126953193+GeekyLiam@users.noreply.github.com>
2024-02-13 18:40:12 +0000

    taxCalc function created

--- AdventuringBank.cpp
+++ AdventuringBank.cpp
@@ -1,20 +1,43 @@
-// AdventuringBank.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <string>
+#include <cmath>
+
+int taxCalc(std::string coinType, int coinAmount) {
+
+    int tax = 0;
+    int copper = 0;
+
+    if (coinType == "Copper") {
+        tax = int(floor(coinAmount * 0.03));
+        return tax;
+    }
+
+    else if (coinType == "Silver") {
+        copper = (coinAmount * 10);
+        tax = int(floor(copper * 0.03));
+        return tax;
+    }
+
+    else if (coinType == "Gold") {
+        copper = (coinAmount * 100);
+        tax = int(floor(copper * 0.03));
+        return tax;
+    }
+
+    else if (coinType == "Platinum") {
+        copper = (coinAmount * 1000);
+        tax = int(floor(copper * 0.03));
+        return tax;
+    }
+
+    else {
+        std::cout << "Error! coinType not recognized.";
+        return 0;
+    }
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int tax = taxCalc("Copper", 101);
+    std::cout << tax;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
