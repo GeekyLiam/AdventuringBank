a254963d0eb9d1476a671933c684a30d8684d7ae
Liam <126953193+GeekyLiam@users.noreply.github.com>
2024-02-14 20:13:58 +0000

    Added game loop

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <chrono>
+#include <thread>
+#include <stdlib.h>
 #include "BankFunctions.h"
 
 int gameLoop() {
@@ -23,17 +26,22 @@ int gameLoop() {
     while (game_run) {
 
         if (i == 7) {
-            int i = 0;
+            i = 0;
         }
 
         std::string current_day = day[i];
-        time_hour += 1 % 24;
+        time_hour += 1;
 
-        if (time_hour == 0) {
+        if (time_hour == 24) {
+            time_hour = 0;
             i++;
         }
 
         std::cout << current_day << ", " << time_hour;
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(200));
+
+        system("CLS");
     }
 
     return 0;
